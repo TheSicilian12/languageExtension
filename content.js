885a6be3baf6826af125ad47209973b3ba59e0b0
TheSicilian12 <mguidera314@gmail.com>
2023-09-15 16:47:59 -0400

    when staticBox is clicked, infoBox appears

--- content.js
+++ content.js
@@ -17,6 +17,21 @@ staticBox.style.left = "10px";
 staticBox.style.zIndex = "9999";
 staticBox.style.cursor = "move";
 
+// Create a div element for the info gather box
+const infoBox = document.createElement("div");
+infoBox.id = "info-box";
+infoBox.style.width = "80px";
+infoBox.style.height = "80px";
+infoBox.style.borderRadius = "15px";
+infoBox.style.border = "1px solid blue";
+infoBox.style.backgroundColor = "blue";
+infoBox.style.position = "fixed";
+infoBox.style.top = "10px";
+infoBox.style.left = "10px";
+infoBox.style.zIndex = "9999";
+infoBox.style.cursor = "move";
+infoBox.style.display = "none";
+
 // Function to handle mouse down event
 const onMouseDown = (e) => {
     isDragging = true;
@@ -39,10 +54,16 @@ const onMouseUp = () => {
     isDragging = false;
 };
 
+const onStaticBoxClick = () => {
+    infoBox.style.display = "block";
+}
+
 // Attach event listeners
 staticBox.addEventListener("mousedown", onMouseDown);
+staticBox.addEventListener("click", onStaticBoxClick);
 document.addEventListener("mousemove", onMouseMove);
 document.addEventListener("mouseup", onMouseUp);
 
 // Append the static box to the body
 document.body.appendChild(staticBox);
+document.body.appendChild(infoBox);
